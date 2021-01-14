1c998a35da22be4733cbe3e39189beed626d940c
Tyler Johnson <tyler@csdconsultants.us>
2021-01-13 20:05:13 -0800

    Created intro to game variable and prompt

--- js/app.js
+++ js/app.js
@@ -9,6 +9,12 @@ var guestName = prompt('Hey welcome! What is your name?');
 console.log(guestName);
 alert('Glad you\'re here ' + guestName + '!');
 
+var firstAnswer = prompt('Would you like to know a little more about me? A simple yes or no will do').toLowerCase();
+console.log(firstAnswer);
+alert('Cool! Let\'s play a guessing game. Guess either yes or no to the following questions.');
+
+var secondAnswer = prompt('')
+
 // if(questionOne === 'yes' || questionOne === 'y'); {
 //   console.log('You are correct!');
 // }
