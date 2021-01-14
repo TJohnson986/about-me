f8fe81cc164727b940e757cb56656dc8babd95b1
Tyler Johnson <tyler@csdconsultants.us>
2021-01-13 20:19:53 -0800

    added if else statement for start game prompt to have a separate reply for any non-yes answer

--- js/app.js
+++ js/app.js
@@ -9,9 +9,16 @@ var guestName = prompt('Hey welcome! What is your name?');
 console.log(guestName);
 alert('Glad you\'re here ' + guestName + '!');
 
-var firstAnswer = prompt('Would you like to know a little more about me? A simple yes or no will do').toLowerCase();
-console.log(firstAnswer);
-alert('Cool! Let\'s play a guessing game. Guess either yes or no to the following questions.');
+var gameStart = prompt('Would you like to know a little more about me? A simple yes or no will do').toLowerCase();
+//console.log gameStart);
+if(gameStart === 'yes' || gameStart === 'y') {
+  console.log('Cool! Let\'s play a guessing game. Guess either yes or no to the following questions.');
+  alert('Cool! Let\'s play a guessing game. Guess either yes or no to the following questions.');
+} else {
+  alert('OK, feel free to come back later.');
+}
+
+
 
 var secondAnswer = prompt('')
 
