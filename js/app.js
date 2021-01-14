97882c03b3a10f3f7fbc727a502fdf1ec077910c
Tyler Johnson <tyler@csdconsultants.us>
2021-01-13 20:28:56 -0800

    added first question prompt including if else statement with separate responses. Corrected console log contents to include description plus response

--- js/app.js
+++ js/app.js
@@ -12,15 +12,19 @@ alert('Glad you\'re here ' + guestName + '!');
 var gameStart = prompt('Would you like to know a little more about me? A simple yes or no will do').toLowerCase();
 //console.log gameStart);
 if(gameStart === 'yes' || gameStart === 'y') {
-  console.log('Cool! Let\'s play a guessing game. Guess either yes or no to the following questions.');
+  console.log('Guest answer if they\'d like to start: ' + gameStart);
   alert('Cool! Let\'s play a guessing game. Guess either yes or no to the following questions.');
 } else {
   alert('OK, feel free to come back later.');
 }
 
-
-
-var secondAnswer = prompt('')
+var firstAnswer = prompt('Let\'s start with an easy one. Do I live in Washington?').toLowerCase();
+if(firstAnswer === 'yes' || firstAnswer === 'y') {
+  console.log('Answer if I live in Washington: ' + firstAnswer);
+  alert('Yep, born and raised!');
+} else {
+  alert('False, so much for an easy one!');
+}
 
 // if(questionOne === 'yes' || questionOne === 'y'); {
 //   console.log('You are correct!');
