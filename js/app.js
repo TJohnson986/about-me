8a1e0b6c32ce7bd7e94aa73d5b3856fbd284819f
Tyler Johnson <tyler@csdconsultants.us>
2021-01-13 19:54:36 -0800

    Created welcome message to capture guest name

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 'use strict';
 
-console.log('hello world');
-
 //write 5 questions. They MUST accept yes or no OR y or n IN ANY CASE.
 //examples: YES, yes, YEs, yeS, Y, y
 
-var questionOne = prompt('Do I live in WA?').toLowerCase();
+//var questionOne = prompt('Do I live in WA?').toLowerCase();
 
-if(questionOne === 'yes' || questionOne === 'y'); {
-  console.log('You are correct!');
-}
+var guestName = prompt('Hey welcome! What is your name?');
+console.log(guestName);
+alert('Glad you\'re here ' + guestName + '!');
+
+// if(questionOne === 'yes' || questionOne === 'y'); {
+//   console.log('You are correct!');
+// }
 
