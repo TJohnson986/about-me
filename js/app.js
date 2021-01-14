4417a7cc39ff8e230313fe8135644297bd47c36f
Tyler Johnson <tyler@csdconsultants.us>
2021-01-13 21:20:24 -0800

    added fourth question and if else statements with separate console logs and response alerts

--- js/app.js
+++ js/app.js
@@ -9,40 +9,48 @@
 // console.log(guestName);
 // alert('Glad you\'re here ' + guestName + '!');
 
-var gameStart = prompt('Would you like to know a little more about me? A simple yes or no will do').toLowerCase();
-//console.log gameStart);
-if(gameStart === 'yes' || gameStart === 'y') {
-  console.log('Guest answer if they\'d like to start: ' + gameStart);
-  alert('Cool! Let\'s play a guessing game. Guess either yes or no to the following questions.');
-} else {
-  alert('OK, feel free to come back later.');
-}
+// var gameStart = prompt('Would you like to know a little more about me? A simple yes or no will do').toLowerCase();
+// if(gameStart === 'yes' || gameStart === 'y') {
+//   console.log('Guest answer if they\'d like to start: ' + gameStart);
+//   alert('Cool! Let\'s play a guessing game. Guess either yes or no to the following questions.');
+// } else {
+//   alert('OK, feel free to come back later.');
+// }
 
-var firstAnswer = prompt('Let\'s start with an easy one. Do I live in Washington?').toLowerCase();
-if(firstAnswer === 'yes' || firstAnswer === 'y') {
-  console.log('Answer if I live in Washington: ' + firstAnswer);
-  alert('Yep, born and raised!');
-} else {
-  console.log('Answer if I live in Washington: ' + firstAnswer);
-  alert('False, so much for an easy one!');
-}
+// var firstAnswer = prompt('Let\'s start with an easy one. Do I live in Washington?').toLowerCase();
+// if(firstAnswer === 'yes' || firstAnswer === 'y') {
+//   console.log('Answer if I live in Washington: ' + firstAnswer);
+//   alert('Yep, born and raised!');
+// } else {
+//   console.log('Answer if I live in Washington: ' + firstAnswer);
+//   alert('False, so much for an easy one!');
+// }
 
-var secondAnswer = prompt('Do I love ice cream? 🍦').toLowerCase();
-if(secondAnswer === 'yes' || secondAnswer === 'y') {
-  console.log('Answer if I love ice cream: ' + secondAnswer);
-  alert('Yes, I very much do!');
-} else {
-  console.log('Answer if I love ice cream: ' + secondAnswer);
-  alert('You\'re crazy, who doesn\'t love ice cream?!');
-}
+// var secondAnswer = prompt('Do I love ice cream? 🍦').toLowerCase();
+// if(secondAnswer === 'yes' || secondAnswer === 'y') {
+//   console.log('Answer if I love ice cream: ' + secondAnswer);
+//   alert('Yes, I very much do!');
+// } else {
+//   console.log('Answer if I love ice cream: ' + secondAnswer);
+//   alert('You\'re crazy, who doesn\'t love ice cream?!');
+// }
 
-var thirdAnswer = prompt('Do I love camping?');
+var thirdAnswer = prompt('Do I love camping?').toLowerCase();
 if(thirdAnswer === 'yes' || thirdAnswer === 'y') {
   console.log('Answer if I love camping: ' + thirdAnswer);
   alert('Yes! It\'s my favorite.');
 } else {
   console.log('Answer if I love camping: ' + thirdAnswer);
   alert('You don\'t know me very well..');
+}
+
+var fourthAnswer = prompt('Do I like mountains more than beaches?').toLowerCase();
+if(fourthAnswer === 'yes' || fourthAnswer === 'y') {
+  console.log('Answer if I like mountains more: ' + fourthAnswer);
+  alert('Yes, mountains are where it\'s at ⛰️');
+} else {
+  console.log('Answer if I like mountains more: ' + fourthAnswer);
+  alert('Nah, mountains are where it\'s at ⛰️');
 }
 
 // if(questionOne === 'yes' || questionOne === 'y'); {
