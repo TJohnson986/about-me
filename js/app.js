61d23133637e4d1cc75e7cc086216a4d5e7afd2d
Tyler Johnson <tyler@csdconsultants.us>
2021-01-16 21:15:39 -0800

    Added score variable and if statement that adds +1 if firstAnswer is yes

--- js/app.js
+++ js/app.js
@@ -7,31 +7,31 @@ var guestName = prompt('Hey welcome! What is your name?');
 //console.log(guestName);
 alert('Glad you\'re here ' + guestName + '!');
 
-// var gameStart = prompt('Would you like to know a little more about me? A simple yes or no will do').toLowerCase();
-// if(gameStart === 'yes' || gameStart === 'y') {
-//   //console.log('Guest answer if they\'d like to start: ' + gameStart);
-//   alert('Cool! Let\'s play a guessing game. Guess using either yes or no to the following questions.');
-// } else {
-//   alert('OK, feel free to come back later.');
-// }
+var gameStart = prompt('Would you like to know a little more about me? A simple yes or no will do').toLowerCase();
+if(gameStart === 'yes' || gameStart === 'y') {
+  //console.log('Guest answer if they\'d like to start: ' + gameStart);
+  alert('Cool! Let\'s play a guessing game. Guess using either yes or no to the following questions.');
+} else {
+  alert('OK, feel free to come back later.');
+}
 
-// var firstAnswer = prompt('Let\'s start with an easy one. Do I live in Washington?').toLowerCase();
-// if(firstAnswer === 'yes' || firstAnswer === 'y') {
-//   //console.log('Answer if I live in Washington: ' + firstAnswer);
-//   alert('Yep, born and raised!');
-// } else {
-//   //console.log('Answer if I live in Washington: ' + firstAnswer);
-//   alert('False, so much for an easy one!');
-// }
+var firstAnswer = prompt('Let\'s start with an easy one. Do I live in Washington?').toLowerCase();
+if(firstAnswer === 'yes' || firstAnswer === 'y') {
+  //console.log('Answer if I live in Washington: ' + firstAnswer);
+  alert('Yep, born and raised!');
+} else {
+  //console.log('Answer if I live in Washington: ' + firstAnswer);
+  alert('False, so much for an easy one!');
+}
 
-// var secondAnswer = prompt('Do I love ice cream? 🍦').toLowerCase();
-// if(secondAnswer === 'yes' || secondAnswer === 'y') {
-//   //console.log('Answer if I love ice cream: ' + secondAnswer);
-//   alert('Yes, I very much do!');
-// } else {
-//   //console.log('Answer if I love ice cream: ' + secondAnswer);
-//   alert('You\'re crazy, who doesn\'t love ice cream?!');
-// }
+var secondAnswer = prompt('Do I love ice cream? 🍦').toLowerCase();
+if(secondAnswer === 'yes' || secondAnswer === 'y') {
+  //console.log('Answer if I love ice cream: ' + secondAnswer);
+  alert('Yes, I very much do!');
+} else {
+  //console.log('Answer if I love ice cream: ' + secondAnswer);
+  alert('You\'re crazy, who doesn\'t love ice cream?!');
+}
 
 // var thirdAnswer = prompt('Do I love camping?').toLowerCase();
 // if(thirdAnswer === 'yes' || thirdAnswer === 'y') {
@@ -80,25 +80,29 @@ alert('Glad you\'re here ' + guestName + '!');
 //   }
 // }
 
-var carColors = ['red',' ' + 'maroon',' ' + 'blue',' ' + 'gold',' ' + 'pink',' ' + 'silver'];
-var myCarColor = carColors[1];
-var seventhAnswer = prompt('Could you guess the color of my car?').toLowerCase();
-var carGuessAttempts = 5;
-while (carGuessAttempts || seventhAnswer !== myCarColor) {
-  console.log(seventhAnswer);
-  console.log(myCarColor);
-  if (seventhAnswer === myCarColor) {
-    console.log('You are correct!');
-    alert ('You are correct! The color of my car is maroon'); break;
-  } else if (seventhAnswer !== myCarColor) {
-    seventhAnswer = prompt(`Nope, that's not it, try a different color! You have ${carGuessAttempts} attempts left`).toLowerCase();
-  }
-  carGuessAttempts--;
-  if (carGuessAttempts === 0) {
-    alert(`Bummer ${guestName}, you ran out of attempts. The correct answer was maroon. The choices I had were ${carColors}.`); break;
-  }
-}
+// var carColors = ['red',' ' + 'maroon',' ' + 'blue',' ' + 'gold',' ' + 'pink',' ' + 'silver'];
+// var myCarColor = carColors[1];
+// var seventhAnswer = prompt('Could you guess the color of my car?').toLowerCase();
+// var carGuessAttempts = 5;
+// while (carGuessAttempts || seventhAnswer !== myCarColor) {
+//   console.log(seventhAnswer);
+//   console.log(myCarColor);
+//   if (seventhAnswer === myCarColor) {
+//     console.log('You are correct!');
+//     alert ('You are correct! The color of my car is maroon'); break;
+//   } else if (seventhAnswer !== myCarColor) {
+//     seventhAnswer = prompt(`Nope, that's not it, try a different color! You have ${carGuessAttempts} attempts left`).toLowerCase();
+//   }
+//   carGuessAttempts--;
+//   if (carGuessAttempts === 0) {
+//     alert(`Bummer ${guestName}, you ran out of attempts. The correct answer was maroon. The choices I had were ${carColors}.`); break;
+//   }
+// }
 
 var score = 0;
+if (firstAnswer === 'yes' || firstAnswer === 'y') {
+  score = (score + 1);
+  console.log(score);
+}
 if ()
 alert('That\'s it! Thanks for playing, ' + guestName + ' and feel free to look around');
