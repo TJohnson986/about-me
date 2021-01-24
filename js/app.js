3b322c9634b2d29edf15d087c75748bcefa00f1d
Tyler Johnson <tyler@csdconsultants.us>
2021-01-23 22:10:47 -0800

    Resolved final syntax errors in question 7. Removed additional unused code. Changed scoring from separate if statements to incrementer in each existing function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var score = 0;
 var guestName = prompt('Hey welcome! What is your name?');
 alert('Glad you\'re here ' + guestName + '!');
 
@@ -14,6 +15,7 @@ function questionOne () {
   var firstAnswer = prompt('Let\'s start with an easy one. Do I live in Washington?').toLowerCase();
   if(firstAnswer === 'yes' || firstAnswer === 'y') {
     alert('Yep, born and raised!');
+    score++;
   } else {
     alert('False, so much for an easy one!');
   }
@@ -23,6 +25,7 @@ function questionTwo() {
   var secondAnswer = prompt('Do I love ice cream? 🍦').toLowerCase();
   if(secondAnswer === 'yes' || secondAnswer === 'y') {
     alert('Yes, I very much do!');
+    score++;
   } else {
     alert('You\'re crazy, who doesn\'t love ice cream?!');
   }
@@ -32,16 +35,17 @@ function questionThree() {
   var thirdAnswer = prompt('Do I love camping?').toLowerCase();
   if(thirdAnswer === 'yes' || thirdAnswer === 'y') {
     alert('Yes! It\'s my favorite.');
+    score++;
   } else {
     alert('You don\'t know me very well..');
   }
 }
 
-
 function questionFour() {
   var fourthAnswer = prompt('Do I like mountains more than beaches?').toLowerCase();
   if(fourthAnswer === 'yes' || fourthAnswer === 'y') {
     alert('Yes, mountains are where it\'s at ⛰️');
+    score++;
   } else {
     alert('Beaches are nice, but mountains are where it\'s at ⛰️');
   }
@@ -49,21 +53,22 @@ function questionFour() {
 
 function questionFive() {
   var fifthAnswer = prompt('Have I been to Alaska?').toLowerCase();
+  if(fifthAnswer === 'yes' || fifthAnswer === 'y') {
     alert('Actually, no I haven\'t, but would really like to go!');
-  } else {
+  }else {
     alert('You\'re right, I haven\'t been but really want to go!');
+    score++;
   }
 }
 
-var correctNumber = 6;
-var guessAttempts = 3;
-var guessedNumber = prompt (`OK ${guestName}, let's try something new. Guess a number between 1 & 10`);
-
 function questionSix() {
+  var correctNumber = 6;
+  var guessAttempts = 3;
+  var guessedNumber = prompt (`OK ${guestName}, let's try something new. Guess a number between 1 & 10`);
   while (guessAttempts && guessedNumber !== correctNumber) {
-    console.log(guessedNumber);
     if (parseInt(guessedNumber) === correctNumber) {
       alert ('You are correct! The number is 6');
+      score++;
       break;
     } else if (parseInt(guessedNumber) < correctNumber) {
       guessedNumber = prompt(`Nope, that's not it, try a higher number! You have ${guessAttempts} attempts left`);
@@ -85,20 +90,16 @@ var carColors = ['dark green','maroon','black','factory','silver'];
 function questionSeven() {
   for (var i = 0; i < carGuessAttempts; i++) {
     var seventhAnswer = prompt(`I have 6 colors in mind to paint my project truck. Could you guess one? You have ${carGuessAttempts} guesses!`).toLowerCase();
-    // for (var j = 0; j < carColors; j++) {
     if (seventhAnswer === carColors[i]) {
       alert('Yeah! That would look awesome!');
+      score++;
       break;
-      // console.log(seventhAnswer);
-    }
-    if (!seventhAnswer) {
-      alert('give me something');
+    } else {
+      alert('No, try again.');
+      carGuessAttempts--;
     }
   }
 }
-
-
-
 
 questionOne();
 questionTwo();
@@ -108,39 +109,4 @@ questionFive();
 questionSix();
 questionSeven();
 
-
-
-
-
-
-// var score = 0;
-// if (firstAnswer === 'yes' || firstAnswer === 'y') {
-//   score = (score + 1);
-//   console.log(score);
-// }
-// if (secondAnswer === 'yes' || secondAnswer === 'y') {
-//   score = (score + 1);
-//   console.log(score);
-// }
-// if (thirdAnswer === 'yes' || thirdAnswer === 'y') {
-//   score = (score + 1);
-//   console.log(score);
-// }
-// if (fourthAnswer === 'yes' || fourthAnswer === 'y') {
-//   score = (score + 1);
-//   console.log(score);
-// }
-// if (fifthAnswer === 'no' || fifthAnswer === 'n') {
-//   score = (score + 1);
-//   console.log(score);
-// }
-// if (parseInt(guessedNumber) === correctNumber) {
-//   score = (score + 1);
-//   console.log(score);
-// }
-// if (seventhAnswer === myCarColor) {
-//   score = (score + 1);
-//   console.log(score);
-// }
-
-// alert(`That's it! Thanks for playing, ${guestName}! You got ${score} out of 7 questions correct. Feel free to look around the rest of my page.`);
+alert(`That's it! Thanks for playing, ${guestName}! You got ${score} out of 7 questions correct. Feel free to look around the rest of my page.`);
